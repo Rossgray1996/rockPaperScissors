92340d8a5a608d61d67e41be674048d3884c4085
Your Name <Rossgray1996@gmail.com>
2022-07-08 11:54:06 +0100

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const scoreBoard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const scissors_div = document.getElementById("s");  
 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
